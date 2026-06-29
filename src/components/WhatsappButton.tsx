23e15036509e0f5bca0beb40a83b671025bd25f8
guillesg <g23.97s@gmail.com>
2026-06-29 17:56:27 +0100

    fixing v1

--- src/components/WhatsappButton.tsx
+++ src/components/WhatsappButton.tsx
@@ -1,9 +1,16 @@
 "use client";
 
-export default function WhatsAppButton() {
+export default function WhatsAppButton({
+  locale,
+}: {
+  locale: "es" | "en";
+}) {
   const phone = "34694253913";
+
   const message = encodeURIComponent(
-    "Hola, me gustaría reservar una cita"
+    locale === "es"
+      ? "Hola, me gustaría reservar una cita."
+      : "Hello, I would like to book an appointment."
   );
 
   return (
@@ -13,10 +20,8 @@ export default function WhatsAppButton() {
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 flex items-center gap-3 bg-[#25D366] text-white px-4 py-3 rounded-full shadow-xl hover:scale-110 transition-all duration-300 animate-[pulse_2.5s_infinite]"
     >
-      {/* ICONO */}
       <span className="text-xl">💬</span>
 
-      {/* TEXTO SOLO DESKTOP */}
       <span className="hidden md:block font-medium">
         WhatsApp
       </span>
